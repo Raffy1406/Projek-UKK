3cc3f84128fc4f0ef40cc1871cf9dcec2e71cd70
Raffy1406 <126653296+Raffy1406@users.noreply.github.com>
2023-03-07 15:15:17 +0700

    init

---
+++ home.php
@@ -0,0 +1,28 @@
+<div class="container vh-100">
+  <h4 class="text-center mt-3">Uji kompetesi Keahlian <br> Rekayasa Perangkat Lunak (RPL)<br> Tahun 2023</h4>
+  <hr>
+  <div class="row mt-3">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header" style="background-color: gold;">INFORMASI</div>
+        <div class="card-body">
+          Selamat datang di Aplikasi pengaduan masyarakat, aplikasi ini dibuat untuk memenuhi tugas Uji Kompetensi Keahlian RPL Tahun 2023 <br><br>
+          Silahkan utarakan keluh kesah anda.
+        </div>
+        <div class="card-footer"></div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card">
+        <div class="card-header" style="background-color: gold;">KONTAK</div>
+        <div class="card-body">
+          Dibuat oleh <br>
+          Nama : Raffy Mochamad Fathur <br>
+          Kelas : XII RPL 1 <br>
+          No.HP : 081297514304 <br>
+        </div>
+        <div class="card-footer"></div>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ layouts/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer mt-3 py-2 fixed-bottom" style="background-color: gold;">
+    <div class="container">
+        <p class="text-center">UKK RPL 2023 | Raffy Mochamad Fathur | SMKN 1 KARAWANG</p>
+    </div>
+</footer>
+
+<script type="text/javascript" src="../assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
